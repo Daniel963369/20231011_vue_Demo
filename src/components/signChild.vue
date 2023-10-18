ab79aff7c1f7f389d5fcc24d975e34f5cbce374c
Daniel963369 <147121432+Daniel963369@users.noreply.github.com>
2023-10-19 06:06:52 +0800

    stage1

--- src/components/signChild.vue
+++ src/components/signChild.vue
@@ -14,16 +14,32 @@ export default {
 
 <template>
 
-<p>{{this.person.name}}</p>
-<p>{{this.person.phone}}</p>
-<p>{{this.person.email}}</p>
-<p>{{this.person.adress}}</p>
-<p>{{this.person.school}}</p>
-<p>{{this.person.gender}}</p>
-<p>{{this.person.interest}}</p>
+<div class="answer">
+    <p>{{this.person.name}}</p>
+    <p>{{this.person.phone}}</p>
+    <p>{{this.person.email}}</p>
+    <p>{{this.person.adress}}</p>
+    <p>{{this.person.school}}</p>
+    <p>{{this.person.gender}}</p>
+    <p>{{this.person.interest}}</p>
+</div>
 
 </template>
 
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+.answer{
+
+    width:80vw;
+    margin:0 10vw;
+    background-color:pink;
+    text-align:center;
+    color:blue;
+    font-size:22pt;
+    border:1px solid black;
+    
+}
+
+</style>
